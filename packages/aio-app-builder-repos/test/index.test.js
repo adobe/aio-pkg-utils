70cbc106a1440be33f7ca94a42dd096fd704ee9a
Amulya Kashyap <amulyak@adobe.com>
2025-01-29 19:03:30 +0530

    Update all dependencies

--- packages/aio-app-builder-repos/test/index.test.js
+++ packages/aio-app-builder-repos/test/index.test.js
@@ -1,25 +1,24 @@
-  const metadata = require('../index')
+const metadata = require('../index')
 
-  test('defaultHost', () => {
-    expect(metadata.defaultHost).toEqual('https://github.com')
-  })
+test('defaultHost', () => {
+  expect(metadata.defaultHost).toEqual('https://github.com')
+})
 
-  test('defaultJiraProject', () => {
-    expect(metadata.defaultJiraProject).toEqual('ACNA')
-  })
+test('defaultJiraProject', () => {
+  expect(metadata.defaultJiraProject).toEqual('ACNA')
+})
 
-  test('repos array', () => {
-    expect(Array.isArray(metadata.repos)).toEqual(true)
-  })
+test('repos array', () => {
+  expect(Array.isArray(metadata.repos)).toEqual(true)
+})
 
-  test('each repo array item must have a \'repo\' property', () => {
-    metadata.repos.forEach(item => expect(item.repo).toBeDefined())
-  })
+test('each repo array item must have a \'repo\' property', () => {
+  metadata.repos.forEach(item => expect(item.repo).toBeDefined())
+})
 
-  test('has an aio-cli repo with components including CLI', () => {
-    const aioCliRepo = metadata.repos.find(item => item.repo === 'adobe/aio-cli')
-    expect(aioCliRepo).toBeDefined()
-    expect(aioCliRepo.components).toBeDefined()
-    expect(aioCliRepo.components.indexOf('CLI')).toBeGreaterThan(-1)
-  })
-  
+test('has an aio-cli repo with components including CLI', () => {
+  const aioCliRepo = metadata.repos.find(item => item.repo === 'adobe/aio-cli')
+  expect(aioCliRepo).toBeDefined()
+  expect(aioCliRepo.components).toBeDefined()
+  expect(aioCliRepo.components.indexOf('CLI')).toBeGreaterThan(-1)
+})
--- packages/aio-batch-update/index.js
+++ packages/aio-batch-update/index.js
@@ -12,7 +12,9 @@
 
 const fs = require('fs').promises
 const execa = require('execa')
+// eslint-disable-next-line node/no-unpublished-require
 const { repos: appBuilderRepos } = require('../aio-app-builder-repos/index')
+// eslint-disable-next-line node/no-missing-require
 const config = require('./update-config.json')
 const ora = require('ora')
 const inquirer = require('inquirer')
@@ -20,66 +22,70 @@ const inquirer = require('inquirer')
 const executable = 'git-xargs'
 
 const main = async () => {
-    const spinner = ora('Running batch update with patch ${config.patch}...').start()
+  const spinner = ora(`Running batch update with patch ${config.patch}...`).start()
 
-    // check if git-xargs is installed
-    try {
-        spinner.text = 'Checking if git-xargs is installed...'
-        await execa(executable, { stdio: 'ignore' })
-        spinner.info('git-xargs is installed')
-    } catch (err) {
-        if (err.message.includes('ENOENT')) {
-            spinner.fail('git-xargs not found. Please install it by using the instructions in the README.md')
-            process.exit(1)
-        }
+  // check if git-xargs is installed
+  try {
+    spinner.text = 'Checking if git-xargs is installed...'
+    await execa(executable, { stdio: 'ignore' })
+    spinner.info('git-xargs is installed')
+  } catch (err) {
+    if (err.message.includes('ENOENT')) {
+      spinner.fail('git-xargs not found. Please install it by using the instructions in the README.md')
+      // eslint-disable-next-line no-process-exit
+      process.exit(1)
     }
+  }
 
-    // check if update patch exists 
-    spinner.text = 'Checking if patch exists...'
-    try {
-        await fs.access(`${__dirname}/patches/${config.patch}`)
-        spinner.info(`Patch ${config.patch} found`)
-    } catch (err) {
-        if (err.code === 'ENOENT') {
-            spinner.fail(`Patch ${config.patch} not found. Exiting...`)
-            process.exit(1)
-        }
+  // check if update patch exists
+  spinner.text = 'Checking if patch exists...'
+  try {
+    // eslint-disable-next-line n/no-path-concat
+    await fs.access(`${__dirname}/patches/${config.patch}`)
+    spinner.info(`Patch ${config.patch} found`)
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      spinner.fail(`Patch ${config.patch} not found. Exiting...`)
+      // eslint-disable-next-line no-process-exit
+      process.exit(1)
     }
+  }
 
-    spinner.text = 'Getting list of repos...'
-    let reposToOperateOn
-    if (config?.repos.length > 0) {
-        spinner.info("Using repos from config...")
-        reposToOperateOn = config.repos
-    } else {
-        spinner.info("No repos specified in config. Using all active and non-documentation repos from aio-app-builder-repos...")
-        reposToOperateOn = appBuilderRepos
-            .filter(repo => !repo.archived && !repo.documentation)
-            .map(repo => repo.repo)
+  spinner.text = 'Getting list of repos...'
+  let reposToOperateOn
+  if (config?.repos.length > 0) {
+    spinner.info('Using repos from config...')
+    reposToOperateOn = config.repos
+  } else {
+    spinner.info('No repos specified in config. Using all active and non-documentation repos from aio-app-builder-repos...')
+    reposToOperateOn = appBuilderRepos
+      .filter(repo => !repo.archived && !repo.documentation)
+      .map(repo => repo.repo)
+  }
+
+  await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'confirm',
+      message: `This patch will be applied to ${reposToOperateOn.length} repos. Do you want to continue?`
     }
+  ]).then(answers => {
+    if (!answers.confirm) {
+      spinner.fail('Patch not applied. Exiting...')
+      // eslint-disable-next-line no-process-exit
+      process.exit(1)
+    }
+  })
 
-    await inquirer.prompt([
-        {
-            type: 'confirm',
-            name: 'confirm',
-            message: `This patch will be applied to ${reposToOperateOn.length} repos. Do you want to continue?`
-        }
-    ]).then(answers => {
-        if (!answers.confirm) {
-            spinner.fail('Patch not applied. Exiting...')
-            process.exit(1)
-        }
-    })
+  spinner.text = 'Writing repos to repos.txt...'
+  await fs.writeFile('repos.txt', reposToOperateOn.join('\n'))
+  spinner.info('Repos written to repos.txt')
 
-    spinner.text = 'Writing repos to repos.txt...'
-    await fs.writeFile('repos.txt', reposToOperateOn.join('\n'))
-    spinner.info('Repos written to repos.txt')
-
-    spinner.text = 'Creating commits and pull requests...'
-    await execa('git-xargs', [
-        '--repos',
-        'repos.txt',
-        "--branch-name",
+  spinner.text = 'Creating commits and pull requests...'
+  await execa('git-xargs', [
+    '--repos',
+    'repos.txt',
+    '--branch-name',
         `${config.branch}`,
         '--commit-message',
         `${config.commitMessage}`,
@@ -90,12 +96,13 @@ const main = async () => {
         '60',
         'git',
         'apply',
-        `${__dirname}/patches/${config.patch}`,
-    ], {
-        stdio: 'inherit'
-    })
-    spinner.info('Commits and pull requests created')
+        // eslint-disable-next-line n/no-path-concat
+        `${__dirname}/patches/${config.patch}`
+  ], {
+    stdio: 'inherit'
+  })
+  spinner.info('Commits and pull requests created')
 }
 
 main()
-    .catch(console.error)
+  .catch(console.error)
